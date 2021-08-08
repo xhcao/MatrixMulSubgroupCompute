2581d69b1527398b14975dabca4ae67dc6785e9d
xhcao <xinghua.cao@intel.com>
2021-08-08 19:35:11 +0800

    Add files via upload

---
+++ MatrixMulSubgroup/MatrixMulSubgroupApp.cpp
@@ -0,0 +1,342 @@
+#include "d3dApp.h"
+#include "MathHelper.h"
+#include "UploadBuffer.h"
+#include "FrameResource.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+
+const int gNumFrameResources = 3;
+
+const int M = 192;
+const int K = 384;
+const int N = 64;
+
+class VecAddComputeApp : public D3DApp
+{
+public:
+	VecAddComputeApp(HINSTANCE hInstance);
+	VecAddComputeApp(const VecAddComputeApp& rhs) = delete;
+	VecAddComputeApp& operator=(const VecAddComputeApp& rhs) = delete;
+	~VecAddComputeApp();
+
+	virtual bool Initialize() override;
+
+private:
+	virtual void OnResize() override;
+	virtual void Update(const GameTimer& gt) override;
+	virtual void Draw(const GameTimer& gt) override;
+	void DoComputeAndDraw(const GameTimer& gt);
+
+	void DoComputeWork();
+
+	void BuildBuffers();
+	void BuildRootSignature();
+	void BuildDescriptorHeaps();
+	void BuildShadersAndInputLayout();
+	void BuildPSOs();
+	void BuildFrameResources();
+
+private:
+	std::vector<std::unique_ptr<FrameResource>> mFrameResources;
+	FrameResource* mCurrFrameResource = nullptr;
+	int mCurrFrameResourceIndex = 0;
+	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
+	ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap = nullptr;
+
+	std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> mGeometries;
+	std::unordered_map<std::string, std::unique_ptr<Material>> mMaterials;
+	std::unordered_map<std::string, std::unique_ptr<Texture>> mTextures;
+	std::unordered_map<std::string, ComPtr<ID3DBlob>> mShaders;
+	std::unordered_map<std::string, ComPtr<ID3D12PipelineState>> mPSOs;
+
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mInputLayout;
+
+	const int NumDataElements = 3200;
+
+	ComPtr<ID3D12Resource> mInputBufferA = nullptr;
+	ComPtr<ID3D12Resource> mInputUploadBufferA = nullptr;
+	ComPtr<ID3D12Resource> mInputBufferB = nullptr;
+	ComPtr<ID3D12Resource> mInputUploadBufferB = nullptr;
+	ComPtr<ID3D12Resource> mOutputBuffer = nullptr;
+	ComPtr<ID3D12Resource> mReadBackBuffer = nullptr;
+
+	PassConstants mMainPassCB;
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
+{
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		VecAddComputeApp theApp(hInstance);
+		if (!theApp.Initialize())
+			return 0;
+
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
+
+VecAddComputeApp::VecAddComputeApp(HINSTANCE hInstance)
+	: D3DApp(hInstance)
+{
+}
+
+VecAddComputeApp::~VecAddComputeApp()
+{
+	if (md3dDevice != nullptr)
+		FlushCommandQueue();
+}
+
+bool VecAddComputeApp::Initialize()
+{
+	if (!D3DApp::Initialize())
+		return false;
+
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+	BuildBuffers();
+	BuildRootSignature();
+	BuildDescriptorHeaps();
+	BuildShadersAndInputLayout();
+	BuildFrameResources();
+	BuildPSOs();
+
+	ThrowIfFailed(mCommandList->Close());
+	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+	FlushCommandQueue();
+	
+	return true;
+}
+
+void VecAddComputeApp::OnResize()
+{
+	D3DApp::OnResize();
+}
+
+void VecAddComputeApp::Update(const GameTimer& gt)
+{
+	mCurrFrameResourceIndex = (mCurrFrameResourceIndex + 1) % gNumFrameResources;
+	mCurrFrameResource = mFrameResources[mCurrFrameResourceIndex].get();
+
+	if (mCurrFrameResource->Fence != 0 && mFence->GetCompletedValue() < mCurrFrameResource->Fence) {
+		HANDLE eventHandle = CreateEventEx(nullptr, nullptr, false, EVENT_ALL_ACCESS);
+		ThrowIfFailed(mFence->SetEventOnCompletion(mCurrFrameResource->Fence, eventHandle));
+		WaitForSingleObject(eventHandle, INFINITE);
+		CloseHandle(eventHandle);
+	}
+}
+
+void VecAddComputeApp::Draw(const GameTimer& gt)
+{
+	DoComputeAndDraw(gt);
+}
+void VecAddComputeApp::DoComputeAndDraw(const GameTimer& gt)
+{
+	DoComputeWork();
+	auto cmdListAlloc = mCurrFrameResource->CmdListAlloc;
+	ThrowIfFailed(cmdListAlloc->Reset());
+
+	ThrowIfFailed(mCommandList->Reset(cmdListAlloc.Get(), mPSOs["opaque"].Get()));
+
+	mCommandList->RSSetViewports(1, &mScreenViewport);
+	mCommandList->RSSetScissorRects(1, &mScissorRect);
+
+	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), (float*)&mMainPassCB.FogColor, 0, nullptr);
+	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL,
+		1.0f, 0, 0, nullptr);
+
+	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
+
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
+		D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+	ThrowIfFailed(mCommandList->Close());
+
+	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+	ThrowIfFailed(mSwapChain->Present(0, 0));
+
+	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
+
+	mCurrFrameResource->Fence = ++mCurrentFence;
+	mCommandQueue->Signal(mFence.Get(), mCurrentFence);
+}
+
+void VecAddComputeApp::DoComputeWork()
+{
+	ThrowIfFailed(mDirectCmdListAlloc->Reset());
+
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), mPSOs["vecAdd"].Get()));
+
+	mCommandList->SetComputeRootSignature(mRootSignature.Get());
+
+	mCommandList->SetComputeRootShaderResourceView(0, mInputBufferA->GetGPUVirtualAddress());
+	mCommandList->SetComputeRootShaderResourceView(1, mInputBufferB->GetGPUVirtualAddress());
+	mCommandList->SetComputeRootUnorderedAccessView(2, mOutputBuffer->GetGPUVirtualAddress());
+
+	mCommandList->Dispatch(1, 1, 1);
+
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mOutputBuffer.Get(),
+		D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_SOURCE));
+
+	ThrowIfFailed(mCommandList->Close());
+
+	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+	FlushCommandQueue();
+
+	float* mappedData = nullptr;
+	ThrowIfFailed(mReadBackBuffer->Map(0, nullptr, reinterpret_cast<void**>(&mappedData)));
+
+	std::ofstream fout("results.txt");
+
+	for (int i = 0; i < NumDataElements; ++i)
+	{
+		fout << "(" << mappedData[i] << ", " << mappedData[i] << ", " << mappedData[i] <<
+			", " << mappedData[i] << ", " << mappedData[i] << ")" << std::endl;
+	}
+
+	mReadBackBuffer->Unmap(0, nullptr);
+}
+
+void VecAddComputeApp::BuildBuffers()
+{
+	// Generate some data.
+	std::vector<float> dataA(M * K);
+	std::vector<float> dataB(K * N);
+	for (int i = 0; i < M; ++i)
+	{
+		for (int j = 0; j < K; j++)
+		{
+			dataA[i * K + j] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+		}
+	}
+
+	for (int i = 0; i < K; ++i)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			dataB[i * N + j] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+		}
+	}
+
+	UINT64 byteSizeA = dataA.size() * sizeof(float);
+	UINT64 byteSizeB = dataB.size() * sizeof(float);
+	UINT64 byteSizeOut = (M * N) * sizeof(float);
+
+	// Create some buffers to be used as SRVs.
+	mInputBufferA = d3dUtil::CreateDefaultBuffer(
+		md3dDevice.Get(),
+		mCommandList.Get(),
+		dataA.data(),
+		byteSizeA,
+		mInputUploadBufferA);
+
+	mInputBufferB = d3dUtil::CreateDefaultBuffer(
+		md3dDevice.Get(),
+		mCommandList.Get(),
+		dataB.data(),
+		byteSizeB,
+		mInputUploadBufferB);
+
+	// Create the buffer that will be a UAV.
+	ThrowIfFailed(md3dDevice->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(byteSizeOut, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS),
+		D3D12_RESOURCE_STATE_UNORDERED_ACCESS,
+		nullptr,
+		IID_PPV_ARGS(&mOutputBuffer)));
+
+	ThrowIfFailed(md3dDevice->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_READBACK),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(byteSizeOut),
+		D3D12_RESOURCE_STATE_COPY_DEST,
+		nullptr,
+		IID_PPV_ARGS(&mReadBackBuffer)));
+}
+
+void VecAddComputeApp::BuildRootSignature()
+{
+	// Root parameter can be a table, root descriptor or root constants.
+	CD3DX12_ROOT_PARAMETER slotRootParameter[3];
+
+	// Perfomance TIP: Order from most frequent to least frequent.
+	slotRootParameter[0].InitAsShaderResourceView(0);
+	slotRootParameter[1].InitAsShaderResourceView(1);
+	slotRootParameter[2].InitAsUnorderedAccessView(0);
+
+	// A root signature is an array of root parameters.
+	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(3, slotRootParameter,
+		0, nullptr,
+		D3D12_ROOT_SIGNATURE_FLAG_NONE);
+
+	// create a root signature with a single slot which points to a descriptor range consisting of a single constant buffer
+	ComPtr<ID3DBlob> serializedRootSig = nullptr;
+	ComPtr<ID3DBlob> errorBlob = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1,
+		serializedRootSig.GetAddressOf(), errorBlob.GetAddressOf());
+
+	if (errorBlob != nullptr)
+	{
+		::OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+	}
+	ThrowIfFailed(hr);
+
+	ThrowIfFailed(md3dDevice->CreateRootSignature(
+		0,
+		serializedRootSig->GetBufferPointer(),
+		serializedRootSig->GetBufferSize(),
+		IID_PPV_ARGS(mRootSignature.GetAddressOf())));
+}
+
+void VecAddComputeApp::BuildDescriptorHeaps()
+{
+
+}
+
+void VecAddComputeApp::BuildShadersAndInputLayout()
+{
+	mShaders["vecAddCS"] = d3dUtil::CompileShader(L"MatrixMulSubgroup.hlsl", nullptr, "CS", "cs_5_0");
+}
+
+void VecAddComputeApp::BuildPSOs()
+{
+	D3D12_COMPUTE_PIPELINE_STATE_DESC computePsoDesc = {};
+	computePsoDesc.pRootSignature = mRootSignature.Get();
+	computePsoDesc.CS =
+	{
+		reinterpret_cast<BYTE*>(mShaders["vecAddCS"]->GetBufferPointer()),
+		mShaders["vecAddCS"]->GetBufferSize()
+	};
+	computePsoDesc.Flags = D3D12_PIPELINE_STATE_FLAG_NONE;
+	ThrowIfFailed(md3dDevice->CreateComputePipelineState(&computePsoDesc, IID_PPV_ARGS(&mPSOs["vecAdd"])));
+}
+
+void VecAddComputeApp::BuildFrameResources()
+{
+	for (int i = 0; i < gNumFrameResources; ++i)
+	{
+		mFrameResources.push_back(std::make_unique<FrameResource>(md3dDevice.Get(),
+			1));
+	}
+}
